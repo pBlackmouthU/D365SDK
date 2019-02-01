581f5c65138bd4c79ec1297cf94995227281c750
pBlackmouth <paco_bocanegra@hotmail.com>
2019-01-31 22:35:17 -0600

    Agregue un nuevo proyectok de consola para los ejemplos simples de operaciones con el IOrganizatioService de Dynamics 365.

---
+++ Consola.OrganizationService/Program.cs
@@ -0,0 +1,19 @@
+﻿using Librerias.Crm.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola.OrganizationService
+{
+    public class Program
+    {
+        private static SimpleConnection365 cnn = null;
+        static void Main(string[] args)
+        {
+            cnn = new SimpleConnection365("");
+
+        }
+    }
+}
